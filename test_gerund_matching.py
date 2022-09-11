6a08fe46df942f7e367e6007ce6bb75395a6550a
sq44 <sq44@duke.edu>
2022-09-11 16:52:07 -0400

    first commit

---
+++ test_gerund_matching.py
@@ -0,0 +1,18 @@
+"""Test name-matching regular expression."""
+import re
+import pytest
+
+# *** ADD YOUR PATTERN BELOW *** #
+pattern = r"\b[a-z]*(?:[b-df-hj-np-tv-xz][aieouy][a-z]*)+ing\b"
+# raise NotImplementedError("Add your pattern to the test file.")
+# *** ADD YOUR PATTERN ABOVE *** #
+
+test_cases = [
+    ("harry loves to sing while showering.", ["showering"]),
+]
+
+
+@pytest.mark.parametrize("string,matches", test_cases)
+def test_name_matching(string, matches):
+    """Test whether pattern correctly matches or does not match input."""
+    assert (re.findall(pattern, string) == matches)
