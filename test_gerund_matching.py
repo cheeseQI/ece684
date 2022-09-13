ebe87056919a4a1ce7fe18b75bd8904776e3793c
sq44 <sq44@duke.edu>
2022-09-13 18:25:19 -0400

    hw1

--- test_gerund_matching.py
+++ test_gerund_matching.py
@@ -4,13 +4,17 @@ import pytest
 
 # *** ADD YOUR PATTERN BELOW *** #
 # pattern = r"\b[a-z]*(?:[b-df-hj-np-tv-xz][aieouy][a-z]*)+ing\b"
-pattern = r"\w+ing"
+pattern = r"\w*(?:[aeiou]|[^aeiou]y)\w*ing"
 # raise NotImplementedError("Add your pattern to the test file.")
 # *** ADD YOUR PATTERN ABOVE *** #
 
 test_cases = [
     # ("harry loves to sing while showering.", ["showering"]),
-     ("harry loves xxx while showering.", ["showering"]),
+    ("harry loves to sing while showering.", ["showering"]),
+    ("The king is doing his thing", ["doing"]),
+    ("he was sleeping while his friend was finding the ring.", ["sleeping", "finding"]),
+    ("He really likes saying before trying it.", ["saying", "trying"]),
+    ("Keep flying", ["flying"])
 ]
 
 
